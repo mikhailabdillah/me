d14cd4e0febed3ea8ce498ecaf51d9aca05557f5
mikhail <mikhailabdillahtr@gmail.com>
2026-08-21 11:08:51 +0700

    feat: add tech stack icons in hero section

--- apps/web/slices/Hero/index.tsx
+++ apps/web/slices/Hero/index.tsx
@@ -10,7 +10,7 @@ const Hero = () => {
         <div className="flex flex-wrap items-center gap-x-6 gap-y-8 md:flex-nowrap">
           <div className="basis-full md:basis-1/2">
             <h1 className="text-5xl">Hey, I&apos;m Mikhail!</h1>
-            <p className="mb-6 text-2xl text-blue-500">Fullstack Developer</p>
+            <p className="mb-6 text-2xl text-blue-500">Full-stack Developer</p>
             <p className="mb-6 text-gray-400 text-lg">
               I specialize in developing high-performance, accessible web
               applications that balance technical excellence with intuitive
@@ -30,6 +30,64 @@ const Hero = () => {
               <span>Hire me</span>
               <IconArrowNarrowRight />
             </Button>
+            <div className="mt-8 flex flex-wrap gap-3">
+              <Image
+                alt="React.js"
+                height={32}
+                src={"/React.png"}
+                title="React.js"
+                width={32}
+              />
+              <Image
+                alt="Next.js"
+                height={32}
+                src={"/nextjs.png"}
+                title="Next.js"
+                width={32}
+              />
+              <Image
+                alt="Node.js"
+                height={32}
+                src={"/Node.png"}
+                title="Node.js"
+                width={32}
+              />
+              <Image
+                alt="Go language"
+                height={32}
+                src={"/Go.png"}
+                title="Go language"
+                width={32}
+              />
+              <Image
+                alt="TypeScript"
+                height={32}
+                src={"/TypeScript.png"}
+                title="TypeScript"
+                width={32}
+              />
+              <Image
+                alt="Shopify"
+                height={32}
+                src={"/shopify.png"}
+                title="Shopify"
+                width={32}
+              />
+              <Image
+                alt="GraphQL"
+                height={32}
+                src={"/GraphQL.png"}
+                title="GraphQL"
+                width={32}
+              />
+              <Image
+                alt="Figma"
+                height={32}
+                src={"/figma.png"}
+                title="Figma"
+                width={32}
+              />
+            </div>
           </div>
           <div className="basis-full md:basis-1/2">
             <div className="mx-auto max-w-sm py-6">
