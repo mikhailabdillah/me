6f977b57ed3d2aac269d5eecdeb5d580ca3c5600
mikhail <mikhailabdillahtr@gmail.com>
2026-08-23 22:50:43 +0700

    feat: add custom link to mdx and add certificates to about page

--- apps/web/app/(Page)/layout.tsx
+++ apps/web/app/(Page)/layout.tsx
@@ -1,6 +1,6 @@
 export default function MdxLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="container mx-auto prose-ol:mb-6 prose-p:mb-4 prose-ul:mb-6 prose-ol:list-disc prose-ul:list-decimal px-4 py-24 prose-ol:pl-6 prose-ul:pl-6 prose-em:font-bold">
+    <div className="container mx-auto prose-ol:mb-6 prose-p:mb-4 prose-ul:mb-6 prose-ol:list-decimal prose-ul:list-disc px-4 py-24 prose-ol:pl-6 prose-ul:pl-6 prose-em:font-bold prose-a:text-blue-600 prose-a:underline">
       {children}
     </div>
   );
--- apps/web/mdx-components.tsx
+++ apps/web/mdx-components.tsx
@@ -1,8 +1,20 @@
 import Image, { type ImageProps } from "next/image";
 
+export default function CustomLink(props: React.ComponentProps<"a">) {
+  const { href } = props;
+  const isInternal = href && (href.startsWith("/") || href.startsWith("#"));
+
+  if (isInternal) {
+    return <a {...props} />;
+  }
+
+  return <a rel="noopener noreferrer" target="_blank" {...props} />;
+}
+
 import type { MDXComponents } from "mdx/types";
 
 const components: MDXComponents = {
+  a: (props) => CustomLink(props),
   img: (props) => (
     <Image
       sizes="100vw"
